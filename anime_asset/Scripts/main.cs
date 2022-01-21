28cb36e9afb8b28d5a577091c8928eb5ee0dffd6
Pavlov Vladimir <pav91pav@mail.ru>
2022-01-21 10:57:23 +0300

    validation ok.make readme and set up rigid body.version 2017.4.1

---
+++ anime_asset/Scripts/main.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+	public Animator animator;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(0, 0, 100, 50), "WALK")){
+			animator.Play ("walk");
+		}
+		if(GUI.Button(new Rect(0, 100, 100, 50), "RUN")){
+			animator.Play ("run");
+		}
+		if(GUI.Button(new Rect(0, 200, 100, 50), "ATTACK")){
+			animator.Play ("attack");
+		}
+		if(GUI.Button(new Rect(0, 300, 100, 50), "IDLE")){
+			animator.Play ("idle");
+		}
+
+	}
+}
